e138c470189d8cb4b3d764b34b30c320a8c7edc4
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-23 17:24:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,28 @@ job_filter = st.selectbox("Select the Gender", pd.unique(df['Gender']))
 
 # creating a single-element container.
 placeholder = st.empty()
+for seconds in range(200):
+#while True: 
+    
+   
+        # create two columns for charts 
+
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'Porn', x = 'Ethnicity')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'Porn')
+            st.write(fig2)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty() 
+
+
+       
 
 # dataframe filter 
 st.sidebar.checkbox("Show Analysis by Ethnicity", True, key=1)
@@ -64,25 +86,3 @@ if st.sidebar.checkbox("Show Analysis by Ethnicity", True, key=2):
 
 # near real-time / live feed simulation 
 
-for seconds in range(200):
-#while True: 
-    
-   
-        # create two columns for charts 
-
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'Porn', x = 'Ethnicity')
-            st.write(fig)
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame = df, x = 'Porn')
-            st.write(fig2)
-        st.markdown("### Detailed Data View")
-        st.dataframe(df)
-        time.sleep(1)
-    #placeholder.empty() 
-
-
-       
