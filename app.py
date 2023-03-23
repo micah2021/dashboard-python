abf93c9dd00d2ec97b7d241ce0fd3ddcca652c2d
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-23 20:27:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,16 +34,7 @@ for seconds in range(1):
    
         # create two columns for charts 
 
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig1 = px.histogram(data_frame = df, x = 'Gender')
-            st.write(fig1)
-       
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'Porn', x = 'Ethnicity')
-            st.write(fig)
+     
 	
 	
 chart_visual = st.sidebar.selectbox('Select Charts/Plot type',
