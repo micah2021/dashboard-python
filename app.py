ed5c184a6d50080b344fa4501cfb27e72b210cf1
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-26 00:06:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,8 @@ ax.hist(df['Gender'], bins=20)
 ax.set(title='A Histogram of App ratings',
        xlabel='Rating out of 5.0',
        ylabel='Count')
-show=plt.show();
-
-st.show
+sh=plt.show();
+st.bar_chart(sh)
 
 
 
