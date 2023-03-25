f471b25be0700a8a58c5827f28925b2f7c25ac4b
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-26 00:35:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,16 +33,16 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 fig, ax = plt.subplots()
 
 # Plot a histogram
-ax.hist(df['Gender'], bins=2)
+ax.hist(df['Gender'], bins=3)
 # Label
 ax.set(title='A Histogram of Gender Count',
        xlabel='Gender',
        ylabel='Count')
 plt.show();
 st.pyplot()
-ax.hist(df['Ethnicity'], bins=2)
+ax1.hist(df['Ethnicity'], bins=3)
 # Label
-ax.set(title='A Histogram of Ethnicity Count',
+ax1.set(title='A Histogram of Ethnicity Count',
        xlabel='Ethnicity',
        ylabel='Porn')
 plt.show();
