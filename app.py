a13e78655a921e6c14d5aa0273ebbb4967156d19
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-25 22:48:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ select1 = st.sidebar.selectbox("Select the Gender", pd.unique(df['Gender']))
 #Plot the unique values
 
 plot=sns.countplot(df['Porn (%)'])
-st.pyplot()
+st.pyplot(plot)
 
 
 # creating a single-element container.
