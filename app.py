b92060f54d1d13dea859c6fa7d7981b58b81e32b
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-28 17:07:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import LabelEncoder
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://docs.google.com/spreadsheets/d/e/2PACX-1vSEIbfyVxix6r_fDNU17bQZzNONVeZYSxPEW3waEve5GmbuSUS5CHKPgVlQkyQo3TQewL9gyodvBdsh/pub?output=csv']
 SERVICE_ACCOUNT_FILE = 'path/to/service_account.json'
 
 creds = service_account.Credentials.from_service_account_file(
