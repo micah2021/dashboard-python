5bb859e22cd04ab18f42d2aa9f624bd31f3f6cbf
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-03-28 22:48:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
 
 # read csv from a github repo
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSEIbfyVxix6r_fDNU17bQZzNONVeZYSxPEW3waEve5GmbuSUS5CHKPgVlQkyQo3TQewL9gyodvBdsh/pub?output=csv")
