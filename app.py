9b4c2142e7a1f72f046d3678744b677ed610e19f
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-04-01 15:15:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def load_data(sheets_url):
     return pd.read_csv(csv_url)
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'dashboard-python/blob/main/serviceaccountfile.json'
+SERVICE_ACCOUNT_FILE = os.path.abspath("serviceaccountfile.json")
+
 
 # Authenticate with Google Sheets API
 creds = None
