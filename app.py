30a382d424edcda519651a166f931a087b4935ac
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-04-02 16:24:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,7 @@ SAMPLE_SPREADSHEET_ID= '1bSihbRkViZF1-pGlX8GrtneDpY_FyASOucCf6IZ14V8'
 
 service=build("sheets", "v4", credentials=creds)
 
-client = gspread.authorize(creds)
-sheet_url="https://docs.google.com/spreadsheets/d/1bSihbRkViZF1-pGlX8GrtneDpY_FyASOucCf6IZ14V8/edit#gid=0"
-sheet = client.open_by_url(sheet_url).sheet1
+sheet=service.spreadsheets()
 data = sheet.get_all_values()
 data = sheet.get_all_values()
 st.table(data)
