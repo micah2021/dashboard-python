87a0da81cc9336f13467f74e600ae227d8de90cf
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-04-02 15:30:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from PIL import Image
 import os
-
+  
 SERVICE_ACCOUNT_FILE = os.path.abspath("key.json")
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
