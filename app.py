0c0689381578bcfa8c30a54a132061e429947aeb
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-04-02 17:29:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,119 +32,117 @@ df= pd.DataFrame(data[1:], columns=data[0])
 
 
 
-def app():
+
 # read csv from a github repo
 	#df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSEIbfyVxix6r_fDNU17bQZzNONVeZYSxPEW3waEve5GmbuSUS5CHKPgVlQkyQo3TQewL9gyodvBdsh/pub?output=csv")
 	
-
-	st.set_page_config(
-    	page_title = 'Lifelight Dashboard',
-    	page_icon = '✅',
-    	layout = 'wide' 
+st.set_page_config(
+page_title = 'Lifelight Dashboard',
+page_icon = '✅',
+layout = 'wide' 
 			)
 	
 # dashboard title
-   
-	st.title("Real-Time Lifelight Dashboard")
-	sh=df.shape
-	if st.button('Check Updates'):
-    		st.write('# The shape of datasets ', sh)
-	else:
-    		st.write('No update of datasets yet')
+st.title("Real-Time Lifelight Dashboard")
+sh=df.shape
+if st.button('Check Updates'):
+    	st.write('# The shape of datasets ', sh)
+else:
+    	st.write('No update of datasets yet')
 # top-level filters 
 	
 	
 	
 	
-	labels = ['Female', 'Male']
+labels = ['Female', 'Male']
 	
 
-	sizes =df['Gender'].value_counts()
+sizes =df['Gender'].value_counts()
 # Create the pie chart using matplotlib
-	fig, ax3 = plt.subplots()
-	ax3.pie(sizes, labels=labels, autopct='%1.1f%%')
-	ax3.axis('equal')
+fig, ax3 = plt.subplots()
+ax3.pie(sizes, labels=labels, autopct='%1.1f%%')
+ax3.axis('equal')
 
 # Display the chart using Streamlit
 
-	fig1, ax1 = plt.subplots()
-	ax1.hist(df['Ethnicity'], bins=5)
+fig1, ax1 = plt.subplots()
+ax1.hist(df['Ethnicity'], bins=5)
 # Label
-	ax1.set(title='A Histogram of Ethnicity Count',
-     	xlabel='Ethnicity',
-     	ylabel='Count')
-	plt.show();
+ax1.set(title='A Histogram of Ethnicity Count',
+xlabel='Ethnicity',
+ylabel='Count')
+plt.show();
 
-	subset = df.loc[df['Ethnicity']=="Others"]
-	fig2, ax4 = plt.subplots()
+subset = df.loc[df['Ethnicity']=="Others"]
+fig2, ax4 = plt.subplots()
 
-	sns.displot(subset["Porn (%)"], color='red')
-	ax4.set_title('Distribution of Total Others Races with Porn')
+sns.displot(subset["Porn (%)"], color='red')
+ax4.set_title('Distribution of Total Others Races with Porn')
 
 # Display the plot using Streamlit
 
 
-	col1, col2, col3= st.columns(3)
-	col1.pyplot(fig)
-	col2.pyplot(fig1)
-	col3.pyplot(fig2)
+col1, col2, col3= st.columns(3)
+col1.pyplot(fig)
+col2.pyplot(fig1)
+col3.pyplot(fig2)
 
 
 
 
-	g = sns.FacetGrid(data=df, col='Ethnicity', col_wrap=2, height=5)
+g = sns.FacetGrid(data=df, col='Ethnicity', col_wrap=2, height=5)
 
 # Map a bar plot of the 'total_bill' column to each subplot
-	g.map(sns.barplot, 'Porn (%)', 'Drawing (%)')
+g.map(sns.barplot, 'Porn (%)', 'Drawing (%)')
 
 # Set the title of each subplot
-	for ax in g.axes.flat:
-    		ax.set_title(f"Porn by Gender and Ethnicity({ax.get_title()})")
+for ax in g.axes.flat:
+    	ax.set_title(f"Porn by Gender and Ethnicity({ax.get_title()})")
 
 # Display the plot using Streamlit
-	st.pyplot(g.fig)
+st.pyplot(g.fig)
 
 
 
-	cross=pd.crosstab(df['Ethnicity'], df['Porn (%)'])
-	st.write("## The is the cross table for Ethnicity with Porn", cross)
+cross=pd.crosstab(df['Ethnicity'], df['Porn (%)'])
+st.write("## The is the cross table for Ethnicity with Porn", cross)
 
-	select1 = st.sidebar.selectbox("Select the Gender", pd.unique(df['Gender']))
+select1 = st.sidebar.selectbox("Select the Gender", pd.unique(df['Gender']))
 	
-	st.set_option('deprecation.showPyplotGlobalUse', False)
-	fig, ax = plt.subplots()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+fig, ax = plt.subplots()
 
 # Plot a histogram
 	
 # creating a single-element container.
-	select1 = st.empty()
+select1 = st.empty()
 #for seconds in range(1):
 #while True: 
-	st.header("The is summary of the datasets")
-	desc=df.describe(include=["bool","object"])
-	desc1=df.describe(include="number")
-	desc
-	desc1
+st.header("The is summary of the datasets")
+desc=df.describe(include=["bool","object"])
+desc1=df.describe(include="number")
+desc
+desc1
      
-	chart_visual = st.sidebar.selectbox('Select Charts/Plot type',('Line Chart', 'Bar Chart', 'Bubble Chart'))
-	st.sidebar.checkbox("Show Analysis by Users", True, key = 1)
-	selected_status = st.sidebar.selectbox('Select Users Status',options = ['Black','Caucasian', 'Mogolian','Others'])
-	fig5 = go.Figure()
+chart_visual = st.sidebar.selectbox('Select Charts/Plot type',('Line Chart', 'Bar Chart', 'Bubble Chart'))
+st.sidebar.checkbox("Show Analysis by Users", True, key = 1)
+selected_status = st.sidebar.selectbox('Select Users Status',options = ['Black','Caucasian', 'Mogolian','Others'])
+fig5 = go.Figure()
 
-	if chart_visual == 'Line Chart':
-		if selected_status == 'Black':
-			fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
+if chart_visual == 'Line Chart':
+	if selected_status == 'Black':
+		fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
 								mode = 'markers',
 								name = 'Black'))
-		if selected_status == 'Caucasian':
-			fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
+	if selected_status == 'Caucasian':
+		fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
 								mode = 'markers', name = 'Caucasian'))
-		if selected_status == 'Mogolian':
-			fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
+	if selected_status == 'Mogolian':
+		fig5.add_trace(go.Scatter(x = df["Porn (%)"], y = df.Ethnicity,
 								mode = 'markers',
 								name = 'Mogolian'))
-		if selected_status == 'Others':   
-			fig5.add_trace(go.Scatter(x=df["Porn (%)"], y=df.Ethnicity,
+	if selected_status == 'Others':   
+		fig5.add_trace(go.Scatter(x=df["Porn (%)"], y=df.Ethnicity,
 								mode='markers',
 								name="Others"))
 
@@ -152,47 +150,47 @@ def app():
 	
 	
 	
-	elif chart_visual == 'Bubble Chart':
-		if selected_status == 'Black':
-			fig5.add_trace(go.Scatter(x=df.Person_Nudity,
+elif chart_visual == 'Bubble Chart':
+	if selected_status == 'Black':
+		fig5.add_trace(go.Scatter(x=df.Person_Nudity,
 								y=df.Ethnicity,
 								mode='markers',
 								marker_size=[40, 60, 80, 60, 40, 50],
 								name='Black'))
 		
-		if selected_status == 'Smoked':
-			fig5.add_trace(go.Scatter(x=data.Person_Nudity, y=df.Ethnicity,
+	if selected_status == 'Smoked':
+		fig5.add_trace(go.Scatter(x=data.Person_Nudity, y=df.Ethnicity,
 								mode='markers',
 								marker_size=[40, 60, 80, 60, 40, 50],
 								name='Caucasian'))
 		
-		if selected_status == 'Mogolian':
-			fig5.add_trace(go.Scatter(x=df.Person_Nudity,
+	if selected_status == 'Mogolian':
+		fig5.add_trace(go.Scatter(x=df.Person_Nudity,
 								y=df.Ethnicity,
 								mode='markers',
 								marker_size=[40, 60, 80, 60, 40, 50],
 								name = 'Mogolian'))
-		if selected_status == 'Others':
-			fig5.add_trace(go.Scatter(x=df.Person_Nudity,
+	if selected_status == 'Others':
+		fig5.add_trace(go.Scatter(x=df.Person_Nudity,
 								y=df.Ethnicity,
 								mode='markers',
 								marker_size=[40, 60, 80, 60, 40, 50],
 								name="Others"))
 
-	st.plotly_chart(fig5, use_container_width=True)
-	st.markdown("### Detailed Data View")
-	st.dataframe(df)
+st.plotly_chart(fig5, use_container_width=True)
+st.markdown("### Detailed Data View")
+st.dataframe(df)
 	
-	def convert_df(df):
+def convert_df(df):
    		 # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    		return df.to_csv().encode('utf-8')
-	csv = convert_df(df)
+    	return df.to_csv().encode('utf-8')
+csv = convert_df(df)
 
-	st.download_button(
-   		label="Download data as CSV",
-    		data=csv,
-    		file_name='lifelight.csv',
-    		mime='text/csv',
+st.download_button(
+   	label="Download data as CSV",
+    	data=csv,
+    	file_name='lifelight.csv',
+    	mime='text/csv',
 			)
     #placeholder.empty() 
 
