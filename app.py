64c61a193c550ef2cd202ff73782f39f53b88012
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-04-03 10:20:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,16 +84,86 @@ ax.set_title("Categorical Data Bar Chart")
 # Display the bar chart using Streamlit
 st.pyplot(fig)
 
-fig, ax= plt.subplots()
-plt.hist(df, bins=30)
+import plotly.graph_objects as go
 
-# Set the chart title and axis labels
-plt.title('Histogram of Sample Data')
-plt.xlabel('S/No')
-plt.ylabel('Gender')
+st.title("Stroke Prediction Dashboard")
+st.markdown("The dashboard will help a researcher to get to know \
+more about the given datasets and it's output")
+st.sidebar.title("Select Visual Charts")
+st.sidebar.markdown("Select the Charts/Plots accordingly:")
 
-# Display the chart using Streamlit
-st.pyplot(fig)
+
+chart_visual = st.sidebar.selectbox('Select Charts/Plot type',
+									('Line Chart', 'Bar Chart', 'Bubble Chart'))
+
+st.sidebar.checkbox("Show Analysis by Smoking Status", True, key = 1)
+selected_status = st.sidebar.selectbox('Select Smoking Status',
+									options = ['Formerly_Smoked',
+												'Smoked', 'Never_Smoked',
+												'Unknown'])
+
+fig = go.Figure()
+
+if chart_visual == 'Line Chart':
+	if selected_status == 'Formerly_Smoked':
+		fig.add_trace(go.Scatter(x = df["Porn (%)"], y = df["Ethnicity"],
+								mode = 'lines',
+								name = 'Formerly_Smoked'))
+	if selected_status == 'Smoked':
+		fig.add_trace(go.Scatter(x = data.Country, y = data.Smokes,
+								mode = 'lines', name = 'Smoked'))
+	if selected_status == 'Never_Smoked':
+		fig.add_trace(go.Scatter(x = data.Country, y = data.Never_Smoked,
+								mode = 'lines',
+								name = 'Never_Smoked'))
+	if selected_status == 'Unknown':
+		fig.add_trace(go.Scatter(x=data.Country, y=data.Unknown,
+								mode='lines',
+								name="Unknown"))
+
+elif chart_visual == 'Bar Chart':
+	if selected_status == 'Formerly_Smoked':
+		fig.add_trace(go.Bar(x=data.Country, y=data.formerly_smoked,
+							name='Formerly_Smoked'))
+	if selected_status == 'Smoked':
+		fig.add_trace(go.Bar(x=data.Country, y=data.Smokes,
+							name='Smoked'))
+	if selected_status == 'Never_Smoked':
+		fig.add_trace(go.Bar(x=data.Country, y=data.Never_Smoked,
+							name='Never_Smoked'))
+	if selected_status == 'Unknown':
+		fig.add_trace(go.Bar(x=data.Country, y=data.Unknown,
+							name="Unknown"))
+
+elif chart_visual == 'Bubble Chart':
+	if selected_status == 'Formerly_Smoked':
+		fig.add_trace(go.Scatter(x=data.Country,
+								y=data.formerly_smoked,
+								mode='markers',
+								marker_size=[40, 60, 80, 60, 40, 50],
+								name='Formerly_Smoked'))
+		
+	if selected_status == 'Smoked':
+		fig.add_trace(go.Scatter(x=data.Country, y=data.Smokes,
+								mode='markers',
+								marker_size=[40, 60, 80, 60, 40, 50],
+								name='Smoked'))
+		
+	if selected_status == 'Never_Smoked':
+		fig.add_trace(go.Scatter(x=data.Country,
+								y=data.Never_Smoked,
+								mode='markers',
+								marker_size=[40, 60, 80, 60, 40, 50],
+								name = 'Never_Smoked'))
+	if selected_status == 'Unknown':
+		fig.add_trace(go.Scatter(x=data.Country,
+								y=data.Unknown,
+								mode='markers',
+								marker_size=[40, 60, 80, 60, 40, 50],
+								name="Unknown"))
+
+st.plotly_chart(fig, use_container_width=True)
+
 
 
 def get_total_dataframe(df):
