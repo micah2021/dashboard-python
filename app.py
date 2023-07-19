a783079768c9b2544708dd5599a03eb28459241a
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-07-19 10:49:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ for seconds in range(5):
 	SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 	creds=None
 	creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-	SPREADSHEET_ID= '1KHSqOAHTGss9ubPMjW2nj8BO70tcsBLtS6A0eqPvhrs'
+	SPREADSHEET_ID= '1bSihbRkViZF1-pGlX8GrtneDpY_FyASOucCf6IZ14V8'
 	service=build("sheets", "v4", credentials=creds)
 	result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range="Brother!A1:O200").execute()
 	# Convert the result to a Pandas DataFrame
