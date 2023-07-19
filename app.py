93031b6f82bf2e8295e552a4be491f569aa49fca
Micah Godwin <78984909+micah2021@users.noreply.github.com>
2023-07-19 13:09:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,10 +42,10 @@ layout = 'wide'
 	
 # dashboard title
 
-st.write("<a href='https://docs.google.com/spreadsheets/d/1KHSqOAHTGss9ubPMjW2nj8BO70tcsBLtS6A0eqPvhrs/edit#gid=0'> Data source </a>", unsafe_allow_html=True)
-st.title("Real-Time Lifelight Dashboard")
-sh=df.shape
-df
+# st.write("<a href='https://docs.google.com/spreadsheets/d/1KHSqOAHTGss9ubPMjW2nj8BO70tcsBLtS6A0eqPvhrs/edit#gid=0'> Data source </a>", unsafe_allow_html=True)
+# st.title("Real-Time Lifelight Dashboard")
+# sh=df.shape
+# df
 if st.button('Check Updates'):
     	st.write('# The shape of datasets ', sh)
 	
